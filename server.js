599f645080ef8faae513b99c1ec34a2783e887b9
George Rodriguez <govintor@icloud.com>
2022-06-23 18:10:44 -0400

    .env added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express ()
 const cors = require ('cors')
 const MongoClient= require ('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
 
 let db,
@@ -16,4 +17,22 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
+    app.use(express.urlencoded({extended:true}))
+    app.use(express.json())
+    app.use(cors())
     
+    app.get('/', async (request, response) => {
+        try {
+            response.render('index.ejs')
+        } catch (error) {
+            response.status(500).send({message: error.message})
+        }
+    })
+    
+    
+    //PORT = 8000
+    app.listen(process.env.PORT || PORT, () => {
+        console.log(`Server is running on port`)
+    })
